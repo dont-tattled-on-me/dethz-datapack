4601d4ce59625e0926721595f6172db8c9f8c7df
Suphakit P <me@suphakit.net>
2023-07-23 19:57:32 +0700

    chore<scripts>: various chnage in build script

--- build.js
+++ build.js
@@ -1,19 +1,21 @@
-let {zip, COMPRESSION_LEVEL} = require('zip-a-folder')
+let { zip, COMPRESSION_LEVEL } = require('zip-a-folder')
 let fs = require('fs')
 
 let config = require('./scripts/config')
 
 class BUILD {
   static async main() {
-    var dir = './' + config.exportFolder;
+    var dir = './' + config.exportFolder
 
-    if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir)
     }
 
-    await zip(config.sourceFolder, config.outputSource, {compression: COMPRESSION_LEVEL.high});
+    await zip(config.sourceFolder, config.outputSource, {
+      compression: COMPRESSION_LEVEL.high,
+    })
 
-    console.log('Build Complete');
+    console.log('Build Complete')
   }
 }
 
