276d808bacd2bddd511cadf4a176794d2bb0123e
Suphakit P <me@suphakit.net>
2023-07-23 19:42:23 +0700

    feat<scripts>: setting up config

---
+++ scripts/config.js
@@ -0,0 +1,17 @@
+let skinName = 'katsuLiminal'
+let exportName = 'dethz-1.20.1'
+
+let sourceFolder = 'src/'
+let exportFolder = 'dist'
+
+
+// Pls Don't touch me :(
+let outputSource = exportFolder + '/' + exportName + '.zip'
+
+module.exports = {
+  skinName,
+  exportName,
+  sourceFolder,
+  exportFolder,
+  outputSource
+}
