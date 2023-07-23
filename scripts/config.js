df7134978d9957a289a3c428155450390b40fad8
Suphakit P <me@suphakit.net>
2023-07-23 19:59:07 +0700

    chore<config>: added version to build config

--- scripts/config.js
+++ scripts/config.js
@@ -1,15 +1,16 @@
-let skinName = 'katsuLiminal'
+let fs = require('fs')
+
 let exportName = 'dethz-1.20.1'
 
 let sourceFolder = 'src/'
 let exportFolder = 'dist'
 
+const fileVersion = JSON.parse(fs.readFileSync(`${__dirname}/../package.json`, 'utf-8')).version
 
 // Pls Don't touch me :(
-let outputSource = exportFolder + '/' + exportName + '.zip'
+let outputSource = exportFolder + '/' + exportName + '-v.' + fileVersion + '.zip'
 
 module.exports = {
-  skinName,
   exportName,
   sourceFolder,
   exportFolder,
