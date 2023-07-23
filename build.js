6484c7b89d20245c34b8a9a993b742908e32125a
Suphakit P <me@suphakit.net>
2023-07-23 19:41:55 +0700

    feat<scripts>: setting up build script

---
+++ build.js
@@ -0,0 +1,20 @@
+let {zip, COMPRESSION_LEVEL} = require('zip-a-folder')
+let fs = require('fs')
+
+let config = require('./scripts/config')
+
+class BUILD {
+  static async main() {
+    var dir = './' + config.exportFolder;
+
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+
+    await zip(config.sourceFolder, config.outputSource, {compression: COMPRESSION_LEVEL.high});
+
+    console.log('Build Complete');
+  }
+}
+
+BUILD.main()
